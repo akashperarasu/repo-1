fd6f8ca47777bb4f2050f9e23cdba1f2bbacc50d
akashdev-7 <alpexnova@gmail.com>
2026-08-29 14:01:41 +0530

    c push

---
+++ main1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+void main(){
+    int a=23;
+    char b='c';
+    double c=24.45;
+    float d=27.25;
+    bool e=true;
+
+    printf("the size of int is:%d \n",sizeof(a));
+    printf("the size of char is:%d \n",sizeof(b));
+    printf("the size of double is:%d \n",sizeof(c));
+    printf("the size of float is:%d \n",sizeof(d));
+    printf("the size of bool is:%d \n",sizeof(e));
+    printf("size of datatype has been declared by akash!!!!");
---
+++ main2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+void main(){
+    int a=23;
+    char b='c';
+    double c=24.45;
+    float d=27.25;
+    bool e=true;
+
+    printf("the adress of a is:%p \n",&a);
+    printf("the adress of b is:%p \n",&b);
+    printf("the adress of c is:%p \n",&c);
+    printf("the adress of d is:%p \n",&d);
+    printf("the adress of e is:%p \n",&e);
+}
